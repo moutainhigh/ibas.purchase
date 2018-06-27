02059ac5136189a48e2592d8e5a0803b21cae85c
niurenzhu <niuren.zhu@icloud.com>
2018-06-27 23:39:41 +0800

    更新配置机制

--- ibas.purchase/src/main/java/org/colorcoding/ibas/purchase/MyConfiguration.java
+++ ibas.purchase/src/main/java/org/colorcoding/ibas/purchase/MyConfiguration.java
@@ -1,9 +1,35 @@
 package org.colorcoding.ibas.purchase;
 
+import org.colorcoding.ibas.bobas.configuration.ConfigurationFactory;
+import org.colorcoding.ibas.bobas.configuration.IConfigurationManager;
+
 /**
  * 我的配置项
  */
 public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration {
+
+	private volatile static IConfigurationManager instance;
+
+	public static IConfigurationManager create() {
+		if (instance == null) {
+			synchronized (MyConfiguration.class) {
+				if (instance == null) {
+					instance = ConfigurationFactory.create().createManager();
+					instance.setConfigSign(MODULE_ID);
+					instance.update();
+				}
+			}
+		}
+		return instance;
+	}
+
+	public static <P> P getConfigValue(String key, P defaultValue) {
+		return create().getConfigValue(key, defaultValue);
+	}
+
+	public static String getConfigValue(String key) {
+		return create().getConfigValue(key);
+	}
 
 	/**
 	 * 模块标识
@@ -29,8 +55,4 @@ public class MyConfiguration extends org.colorcoding.ibas.bobas.MyConfiguration 
 	 * 服务命名空间
 	 */
 	public static final String NAMESPACE_SERVICE = NAMESPACE_ROOT + "service";
-	/**
-	 * 配置项目-文件文件夹
-	 */
-	public final static String CONFIG_ITEM_PURCHASE_FILE_FOLDER = "PHFileFolder";
 }
