d477553fd6c62afb4d562d8323b1d62fcf4558ac
牛加人等于朱 <niuren.zhu@icloud.com>
2018-07-30 17:31:50 +0800

    优化代码

--- ibas.purchase.service/src/main/java/org/colorcoding/ibas/purchase/service/rest/Resolver.java
+++ ibas.purchase.service/src/main/java/org/colorcoding/ibas/purchase/service/rest/Resolver.java
@@ -6,11 +6,14 @@ import javax.ws.rs.ext.Provider;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 
-import org.colorcoding.ibas.bobas.bo.*;
-import org.colorcoding.ibas.bobas.common.*;
-import org.colorcoding.ibas.purchase.bo.purchasedelivery.*;
-import org.colorcoding.ibas.purchase.bo.purchaseorder.*;
-import org.colorcoding.ibas.purchase.bo.purchasereturn.*;
+import org.colorcoding.ibas.bobas.bo.UserFieldProxy;
+import org.colorcoding.ibas.bobas.common.Criteria;
+import org.colorcoding.ibas.bobas.common.OperationResult;
+import org.colorcoding.ibas.purchase.bo.purchasedelivery.PurchaseDelivery;
+import org.colorcoding.ibas.purchase.bo.purchaseorder.PurchaseOrder;
+import org.colorcoding.ibas.purchase.bo.purchasequote.PurchaseQuote;
+import org.colorcoding.ibas.purchase.bo.purchasereturn.PurchaseReturn;
+import org.colorcoding.ibas.purchase.bo.shippingaddress.ShippingAddress;
 
 /**
  * 序列化解释器
@@ -19,22 +22,19 @@ import org.colorcoding.ibas.purchase.bo.purchasereturn.*;
 @Produces({ "application/json" })
 public class Resolver implements ContextResolver<JAXBContext> {
 
-    private static JAXBContext jaxbContext = null;
+	private static JAXBContext jaxbContext = null;
 
-    public JAXBContext getContext(Class<?> type) {
-        try {
-            if (jaxbContext == null) {
-                jaxbContext = JAXBContext.newInstance(Criteria.class, OperationResult.class
-                    , UserFieldProxy.class
-                    , PurchaseDelivery.class
-                    , PurchaseOrder.class
-                    , PurchaseReturn.class
-                );
-            }
-        } catch (JAXBException e) {
-            e.printStackTrace();
-        }
-        return jaxbContext;
-    }
+	public JAXBContext getContext(Class<?> type) {
+		try {
+			if (jaxbContext == null) {
+				jaxbContext = JAXBContext.newInstance(Criteria.class, OperationResult.class, UserFieldProxy.class,
+						PurchaseDelivery.class, PurchaseOrder.class, PurchaseReturn.class, PurchaseQuote.class,
+						ShippingAddress.class);
+			}
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		return jaxbContext;
+	}
 
 }
